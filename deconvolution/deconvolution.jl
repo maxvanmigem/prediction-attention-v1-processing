71b06ff4888324730254e2e95b1b1b10fbed7693
Max Van Migem <m.van.migem@gmail.com>
2024-11-04 14:55:12 +0100

    implementation of unfold overlap correction

--- deconvolution/deconvolution.jl
+++ deconvolution/deconvolution.jl
@@ -5,38 +5,61 @@ using CSV
 using DataFrames
 using Effects
 using StatsModels
+using Printf
 
-data_input = "C:/Users/mvmigem/Documents/data/project_1/preprocessed/mastoid_ref_csv/raw_mastoidref_21.csv"
-events_input = "C:/Users/mvmigem/Documents/data/project_1/preprocessed/mastoid_ref_csv/events_21.csv"
-data = DataFrame(CSV.File(data_input))
-evts = DataFrame(CSV.File(events_input))
-rename!(evts,:sample => :latency)
-filter!(row -> !(row.event_codes == 99),evts)
-evts.position = string.(evts.position)
-evts.sequence = string.(evts.sequence)
-data_1, evts_1 = UnfoldSim.predef_eeg()
+# Data paths
+data_path = "C:/Users/mvmigem/Documents/data/project_1/preprocessed/mastoid_ref_csv/"
+# destination_path = "C:/Users/mvmigem/Documents/data/project_1/overlap_corrected/fixed-electrode/"
+destination_path = "C:/Users/mvmigem/Documents/data/project_1/overlap_corrected/variable-electrode/"
+event_path = data_path * "events/"
+# raw_path = data_path * "raw-POz/"
+raw_path = data_path * "raw-selected/"
 
-select!(data,"POz")
+event_dir_list = readdir(event_path)
+raw_dir_list = readdir(raw_path)
 
-data_r = vec(Matrix(data))
+for (evp,rawp) in zip(event_dir_list,raw_dir_list)
 
-basisfunction = firbasis(τ=(-0.4,0.8),sfreq=128,name="myFIRbasis")
+    # Read in the data
+    data = DataFrame(CSV.File(raw_path*rawp))
+    evts = DataFrame(CSV.File(event_path*evp))
+    # Change the column names to fit toolbox
+    rename!(evts,:sample => :latency)
+    filter!(row -> !(row.event_codes == 99),evts)
+    evts.position = string.(evts.position)
+    evts.sequence = string.(evts.sequence)
+    
+    # selected electrode
+    # selected_electrode = "POz"
+    selected_electrode = string(evts[1,:selected_electrode])
+    select!(data,selected_electrode)
+    # Transform to data to Matrix
+    data_r = vec(Matrix(data))
+    # Define basisfunction
+    basisfunction = firbasis(τ=(-0.1,1),sfreq=512,name="myFIRbasis")
+    # Define the linear formula 
+    f = @formula 0 ~ 1 + position + sequence + expectation*attention 
+    # f = @formula 0 ~ 1 + condition + continuous # note the formulas left side is `0 ~ ` for technical reasons`
+    bfDict = Dict(Any=>(f,basisfunction))
+    # Specify contrasts
+    contrasts = Dict(:position=>EffectsCoding(),
+                    :sequence=>EffectsCoding(),
+                    :expectation=>EffectsCoding(),
+                    :attention=>EffectsCoding())
+    # Fit
+    m = fit(UnfoldModel,bfDict,evts,data_r,contrasts=contrasts)
 
-f = @formula 0 ~ 1 + position + sequence + expectation + attention + expectation&attention
-# f = @formula 0 ~ 1 + condition + continuous # note the formulas left side is `0 ~ ` for technical reasons`
-bfDict = Dict(Any=>(f,basisfunction))
-contrasts = Dict(:position=>EffectsCoding(),
-                :sequence=>EffectsCoding(),
-                :expectation=>EffectsCoding(),
-                :attention=>EffectsCoding())
-m = fit(UnfoldModel,bfDict,evts,data_r,contrasts=contrasts)
+    setup = string(evts[1,:setup])
+    setdown = string(evts[1,:setdown])
 
-results = coeftable(m)
-plot_erp(results)
+    # design = Dict(:sequence =>["2"],:position => ["1","3"] ,:expectation => ["regular","odd"])
+    design = Dict(:sequence =>["2"],:position => [setup,setdown] ,:attention => ["attended","unattended"],:expectation => ["regular","odd"])
+    eff = effects(design,m)
+    eff.subject .= string(evts[1,:subject])
+    eff.selected_electrode .= selected_electrode
+    # Save evoked
+    subject_num = lpad(evts[1,:subject],2,"0")
+    filename = "corrected_evoked_$subject_num.csv"
+    CSV.write(destination_path*filename, eff)
 
-design = Dict(:sequence => ["2"],:expectation => ["regular","odd"], :attention => ["attended","unattended"])
-eff = effects(design,m)
-plot_erp(eff; mapping = (; color = :attention))
-
-
-
+end
--- deconvolution/test_julia.jl
+++ deconvolution/test_julia.jl
@@ -1,33 +1,11 @@
-using DataFrames
-using Unfold
-using UnfoldMakie, CairoMakie # for plotting
-using UnfoldSim
 
-data, evts = UnfoldSim.predef_eeg()
+data_path = "C:/Users/mvmigem/Documents/data/project_1/preprocessed/mastoid_ref_csv/"
+event_path = data_path * "events/"
+raw_path = data_path * "raw-POz/"
 
-times_cont = range(0,length=200,step=1/100) # we simulated with 100hz for 0.5 seconds
+event_dir_list = readdir(event_path)
+raw_dir_list = readdir(raw_path)
 
-f,ax,h = plot(times_cont,data[1:200])
-vlines!(evts[evts.latency .<= 200, :latency] ./ 100;color=:black) # show events, latency in samples!
-ax.xlabel = "time [s]"
-ax.ylabel = "voltage [µV]"
-f
-
-show(first(evts, 6), allcols = true)
-
-# Unfold supports multi-channel, so we could provide matrix ch x time, which we can create like this from a vector:
-data_r = reshape(data, (1,:))
-# cut the data into epochs
-data_epochs, times = Unfold.epoch(data = data, tbl = evts, τ = (-0.4, 0.8), sfreq = 100); # channel x timesteps x trials
-size(data_epochs)
-
-typeof(data_epochs)
-
-f = @formula 0 ~ 1 + condition + continuous # note the formulas left side is `0 ~ ` for technical reasons`
-
-m = fit(UnfoldModel, f, evts, data_epochs, times);
-
-m
-
-first(coeftable(m), 6)
-
+for i in raw_dir_list
+    println(i)
+end
