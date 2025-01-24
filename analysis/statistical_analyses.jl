707e79b44382595632eefa38342c6ebe7eca77df
Max Van Migem <m.van.migem@gmail.com>
2025-01-24 12:11:06 +0100

    added baysion modell

--- analysis/statistical_analyses.jl
+++ analysis/statistical_analyses.jl
@@ -4,7 +4,7 @@ using StatsModels
 using CSV
 using CategoricalArrays
 
-df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_long_20ms_df.csv"
+df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_long_df.csv"
 
 df = DataFrame(CSV.File(df_path))
 
@@ -15,6 +15,7 @@ df.visual_field = categorical(df.visual_field)
 
 df_up = filter(row -> row.visual_field == "up", df)
 df_down = filter(row -> row.visual_field == "down", df)
+df_expected = filter(row -> row.expectation == "expected", df)
 
 contrasts = Dict(:attention=>EffectsCoding(),
                 :visual_field=>EffectsCoding(),
@@ -23,17 +24,18 @@ contrasts = Dict(:attention=>EffectsCoding(),
 
 
 # fit up and down tailored_amp ultra_amp general_amp
-f = @formula(general_amp ~ 1 + attention + expectation + visual_field +
-    attention&expectation + attention&visual_field + expectation&visual_field +
+f = @formula(general_amp ~ 1 + attention*expectation*visual_field +
     (1 + attention | subject) + (expectation | subject))
 
-fit_all = fit(MixedModel,f,df,contrasts=contrasts)
+fit_all = fit(MixedModel,f,df,REML = true,contrasts=contrasts)
 
 # fit only up tailored_amp ultra_amp general_amp
-f = @formula(tailored_amp ~ 1 + attention + expectation+ attention&expectation +
+f = @formula(general_amp ~ 1 + attention*expectation +
     (1 + attention | subject) + (expectation | subject))
 
 fit_up = fit(MixedModel,f,df_up,contrasts=contrasts)
 
 # fit only down tailored_amp ultra_amp general_amp
 fit_down = fit(MixedModel,f,df_down,contrasts=contrasts)
+
+
