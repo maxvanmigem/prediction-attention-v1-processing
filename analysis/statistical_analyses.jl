c52c023798568090268d1425279cbd3c26c6de37
Max Van Migem <m.van.migem@gmail.com>
2025-02-05 21:45:09 +0100

    big dump

--- analysis/statistical_analyses.jl
+++ analysis/statistical_analyses.jl
@@ -4,7 +4,16 @@ using StatsModels
 using CSV
 using CategoricalArrays
 
-df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_long_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_long_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/p3_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_long_nosubset_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_galat_long_df.csv"
+df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_oc_fix_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/c1_oc_var_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/p3a_oc_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/p3b_oc_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/p1_oc_df.csv"
+# df_path = "C:/Users/mvmigem/Documents/data/project_1/compiled_dataframes/p2_oc_df.csv"
 
 df = DataFrame(CSV.File(df_path))
 
@@ -23,14 +32,14 @@ contrasts = Dict(:attention=>EffectsCoding(),
                 :subject=>EffectsCoding())
 
 
-# fit up and down tailored_amp ultra_amp general_amp
-f = @formula(general_amp ~ 1 + attention*expectation*visual_field +
+# fit up and down tailored_amp ultra_amp general_amp baseline_corrected
+f = @formula(baseline_corrected ~ 1 + attention*expectation*visual_field +
     (1 + attention | subject) + (expectation | subject))
 
 fit_all = fit(MixedModel,f,df,REML = true,contrasts=contrasts)
 
 # fit only up tailored_amp ultra_amp general_amp
-f = @formula(general_amp ~ 1 + attention*expectation +
+f = @formula(tailored_amp ~ 1 + attention*expectation +
     (1 + attention | subject) + (expectation | subject))
 
 fit_up = fit(MixedModel,f,df_up,contrasts=contrasts)
